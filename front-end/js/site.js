768561af641f2421574ca110d5b3aac2279e8fbf
georgeBl <blanarugeorge96@gmail.com>
2025-02-27 13:50:54 +0000

    Added library and removed stud

--- front-end/js/site.js
+++ front-end/js/site.js
@@ -1,16 +1,18 @@
 function init() {
     console.log('init');
-  
-       // Event listener for form submission
-       document.getElementById("studentForm").addEventListener("submit", addStudent);
-    FetchStudents()
+
+    // Event listener for form submission
+    // document.getElementById("studentForm").addEventListener("submit", addStudent);
+    document.getElementById("libraryForm").addEventListener("submit", addLibrary);
+   // FetchStudents()
+    FetchLibrary()
 }
 
 async function FetchStudents() {
     console.log('FetchStudents')
     try {
         const response = await fetch('https://localhost:7276/api/Student');
-       
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -21,29 +23,70 @@ async function FetchStudents() {
 
         const students = await response.json();
 
-        students.forEach(student => { 
+        students.forEach(student => {
 
-                const row = document.createElement('tr');
-                row.innerHTML = `
+            const row = document.createElement('tr');
+            row.innerHTML = `
                 <td>${student.id}</td>
                 <td>${student.name}</td>
                 <td>${student.email}</td>
                 <td>${student.age}</td>
                 <td>${student.course}</td>`
-                tableBody.appendChild(row);
+            tableBody.appendChild(row);
 
         });
 
 
-        console.log({students});
+        console.log({ students });
 
     } catch (error) {
 
         console.error(`HTTP error! status: ${response.status}`);
 
-     }
+    }
 
 
 
 }
 
+
+async function FetchLibrary() {
+    console.log('FetchLibrary')
+    try {
+        const response = await fetch('https://localhost:7276/api/Library');
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const tableBody = document.querySelector('#libraryTable tbody');
+
+        tableBody.innerHTML = '';
+
+        const students = await response.json();
+
+        students.forEach(student => {
+
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${student.id}</td>
+                <td>${student.name}</td>
+                <td>${student.email}</td>
+                <td>${student.age}</td>
+                <td>${student.course}</td>`
+            tableBody.appendChild(row);
+
+        });
+
+
+        console.log({ students });
+
+    } catch (error) {
+
+        console.error(`HTTP error! status: ${response.status}`);
+
+    }
+
+
+
+}
